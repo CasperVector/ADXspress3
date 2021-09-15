f5b6df5e4a72a24b0540ff943a1afa2e722151d8
Casper Ti. Vector <CasperVector@gmail.com>
2021-09-15 18:41:31 +0800

    Use NDPluginAttribute for iocXspress3.

--- iocs/xspress3IOC/iocBoot/iocXspress3/xsp3-chan.sh
+++ iocs/xspress3IOC/iocBoot/iocXspress3/xsp3-chan.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -
 
 [ "$1" -gt 0 ] || exit 1
+d="${2:-"$(dirname "$0")"}"
 
 exec > xsp3-"$1"ch-batch.cmd
 for i in $(seq "$1"); do
@@ -15,4 +16,12 @@ for i in $(seq "$1"); do
 	printf 'file "xsp3_chan.req", P=$(P), CHAN=%d\n' "$i"
 done
 printf '\n'
+
+exec > xsp3-"$1"ch.xml
+printf '<?xml version="1.0"?>\n<Attributes>\n'
+for i in $(seq "$1"); do
+	sed 's/\${CHAN}/'"$i"'/g; s/\${ADDR}/'"$(expr "$i" - 1)"'/' \
+		"$d"/chan-attr.xml
+done
+printf '</Attributes>\n\n'
 
