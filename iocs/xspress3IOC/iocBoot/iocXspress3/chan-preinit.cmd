8a866667baf30fe86030a78d477103857ddb495f
Casper Ti. Vector <CasperVector@gmail.com>
2021-09-19 17:07:26 +0800

    iocXspress3: simplify xsp3-chan.substitutions.

--- iocs/xspress3IOC/iocBoot/iocXspress3/chan-preinit.cmd
+++ iocs/xspress3IOC/iocBoot/iocXspress3/chan-preinit.cmd
@@ -5,5 +5,5 @@ NDStdArraysConfigure("MCASUM$(CHAN)", 20, 0, "ROISUM$(CHAN)", 0, 0, 0, 0, 0, 5)
 NDAttrConfigure("ATTR$(CHAN)", $(QSIZE), 0, "$(PORT)", 0, 10)
 NDTimeSeriesConfigure("ATTR$(CHAN)_TS", $(QSIZE), 0, "ATTR$(CHAN)", 0, 10)
 NDROIStatConfigure("ROISTAT$(CHAN)", $(QSIZE), 0, "ROI$(CHAN)", 0, 16, 0, 0, 0, 0, 1)
-dbLoadTemplate("$(TOP)/iocBoot/$(IOC)/xsp3-chan.substitutions", "P_=$(PREFIX),MAXFRAMES=$(MAXFRAMES),NUMBINS=$(NUMBINS),PORT_=$(PORT),ADDR_=$(ADDR),CHAN=$(CHAN)")
+dbLoadTemplate("$(TOP)/iocBoot/$(IOC)/xsp3-chan.substitutions", "P=$(PREFIX),PORT=$(PORT),ADDR=$(ADDR),MAXFRAMES=$(MAXFRAMES),NUMBINS=$(NUMBINS),CHAN=$(CHAN)")
 
