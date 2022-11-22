1045805967ae950f7a127ec6ebcc649171225054
Casper Ti. Vector <CasperVector@gmail.com>
2022-11-22 23:56:13 +0800

    chan-preinit.cmd: follow EXAMPLE_commonPlugins.cmd closer.

--- iocs/xspress3IOC/iocBoot/iocXspress3/chan-preinit.cmd
+++ iocs/xspress3IOC/iocBoot/iocXspress3/chan-preinit.cmd
@@ -3,7 +3,7 @@ NDStdArraysConfigure("MCA$(CHAN)", 20, 0, "ROI$(CHAN)", 0, 0, 0, 0, 0, 5)
 NDROIConfigure("ROISUM$(CHAN)", $(QSIZE), 0, "$(PORT)", 0, 0, 0, 0, 0, 1)
 NDStdArraysConfigure("MCASUM$(CHAN)", 20, 0, "ROISUM$(CHAN)", 0, 0, 0, 0, 0, 5)
 NDAttrConfigure("ATTR$(CHAN)", $(QSIZE), 0, "$(PORT)", 0, 10)
-NDTimeSeriesConfigure("ATTR$(CHAN)_TS", $(QSIZE), 0, "ATTR$(CHAN)", 0, 10)
+NDTimeSeriesConfigure("ATTR$(CHAN)_TS", $(QSIZE), 0, "ATTR$(CHAN)", 1, 10)
 NDROIStatConfigure("ROISTAT$(CHAN)", $(QSIZE), 0, "ROI$(CHAN)", 0, 16, 0, 0, 0, 0, 1)
 dbLoadTemplate("$(TOP)/iocBoot/$(IOC)/xsp3-chan.substitutions", "P=$(PREFIX),PORT=$(PORT),ADDR=$(ADDR),MAXFRAMES=$(MAXFRAMES),NUMBINS=$(NUMBINS),CHAN=$(CHAN)")
 
