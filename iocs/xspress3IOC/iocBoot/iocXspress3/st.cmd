024f80b84458ac2063e31e18e72e427da0d3b804
Casper Ti. Vector <CasperVector@gmail.com>
2025-01-29 22:17:45 +0800

    xsp3.template, iocXspress3: multiple cosmetics.

--- iocs/xspress3IOC/iocBoot/iocXspress3/st.cmd
+++ iocs/xspress3IOC/iocBoot/iocXspress3/st.cmd
@@ -17,7 +17,7 @@ epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db:$(ADXSPRESS3)/db")
 
 xspress3Config("$(PORT)", "$(XSP3CHANS)", "$(XSP3CARDS)", "$(XSP3ADDR)", "$(MAXFRAMES)", "$(MAXFRAMES)", "$(NUMBINS)", 0, 0, 0, 0)
 dbLoadRecords("xsp3.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-NDFileHDF5Configure("FileHDF1", $(QSIZE), 0, "$(PORT)", 0)
+NDFileHDF5Configure("FileHDF1", $(QSIZE), 0, "$(PORT)", 0, 0)
 dbLoadRecords("NDFileHDF5.template", "P=$(PREFIX),R=HDF1:,PORT=FileHDF1,ADDR=0,TIMEOUT=1,XMLSIZE=2048,NDARRAY_PORT=$(PORT)")
 NDProcessConfigure("PROC1", $(QSIZE), 0, "$(PORT)", 0, 0, 0)
 dbLoadRecords("NDProcess.template", "P=$(PREFIX),R=Proc1:,PORT=PROC1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT)")
@@ -48,5 +48,7 @@ dbpf("$(PREFIX)cam1:CONNECT", 1)
 dbpf("$(PREFIX)cam1:CTRL_DTC", "Disable")
 dbpf("$(PREFIX)cam1:EraseOnStart", "Yes")
 dbpf("$(PREFIX)cam1:TriggerMode", "Internal")
+dbpf("$(PREFIX)cam1:AcquireTime", 0.02)
+dbpf("$(PREFIX)cam1:NumImages", 100)
 epicsThreadSleep 2
 
